bd5320b67b7af205acaf1000171788aabebd467b
iisakki <ikamula@topcon.com>
2024-03-19 14:14:48 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cors-test';
+
+  constructor(private http: HttpClient){
+    this.http.get('https://google.com').pipe(first()).subscribe((data) => {console.log(data)} );
+  }
 }
